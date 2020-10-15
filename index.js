af4f9c00d419a7aaf8bf8bb5c65f29f86ddb8293
Scrag <fogarasy.roland@gmail.com>
2020-10-15 18:31:20 +0200

    improvements

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const spawnSync = require('child_process').spawnSync
+const spawnSync = require('child_process').spawn
 const path = require('path')
 
-module.exports = function(content) {
-  const twig = spawnSync(path.resolve(__dirname , './phptotwig.php'), [], { input : content }).stdout.toString()
-  return twig
+module.exports = function(content, map, meta) {
+  this.cacheable();
+  var callback = this.async();
+  const twig = spawnSync(path.resolve(__dirname , './phptotwig.php'), [])
+  twig.stdin.write(content);
+  twig.stdin.end();
+  twig.stdout.on('data', (data) => {
+    callback(null, "module.exports = " + JSON.stringify(data.toString()), map, meta)
+    // callback(null, data, map, meta)
+  });
+  twig.stderr.on('data', (data) => {
+    console.error(data.toString());
+
+    callback(null, data, map, meta)
+  });
+  twig.on('close', (code) => {
+
+  });
 }
+
+module.exports.seperable = true;
